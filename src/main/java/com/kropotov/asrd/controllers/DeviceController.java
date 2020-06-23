a93f7879fe98eeb1a229b155c5c1a3546e2729b8
Gribnoff <50702390+Gribnoff@users.noreply.github.com>
2020-06-23 16:37:05 +0300

    criteria-search. Поиск для приборов

--- src/main/java/com/kropotov/asrd/controllers/DeviceController.java
+++ src/main/java/com/kropotov/asrd/controllers/DeviceController.java
@@ -19,12 +19,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @Controller
@@ -44,8 +47,31 @@ public class DeviceController {
     private final DtoToDevice dtoToDevice;
 
     @GetMapping
-    public String displayDevices(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
-        PageWrapper<Device> page = new PageWrapper<>(deviceService.getAll(pageable.previousOrFirst()), "/devices");
+    public String displayDevices(Model model,
+                                 @RequestParam(required = false) Byte entityStatus,
+                                 @RequestParam(required = false) String system,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime createdAtFrom,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime createdAtTo,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime updatedAtFrom,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime updatedAtTo,
+                                 @RequestParam(required = false) String number,
+                                 @RequestParam(required = false) Byte location,
+                                 @RequestParam(required = false) String purpose,
+                                 @RequestParam(required = false) String purposePassport,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate vintageFrom,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate vintageTo,
+                                 @RequestParam(required = false) Integer vpNumber,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate otkDateFrom,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate otkDateTo,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate vpDateFrom,
+                                 @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate vpDateTo,
+                                 @RequestParam(required = false) String title,
+                                 @RequestParam(required = false) String userName,
+                                 @PageableDefault(page = 0, size = 15, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        PageWrapper<Device> page = new PageWrapper<>(deviceService.getAll(
+                entityStatus, system, createdAtFrom, createdAtTo, updatedAtFrom, updatedAtTo, number, location, purpose, purposePassport,
+                vintageFrom, vintageTo, vpNumber, otkDateFrom, otkDateTo, vpDateFrom, vpDateTo, title, userName,
+                pageable.previousOrFirst()), "/devices");
 
         PageValues.addContentToModel(model, page);
         model.addAttribute("topicTitleList", topicService.getAll().orElse(new ArrayList<>()));
